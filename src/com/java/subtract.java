cb0279f9753039d6976ff2d23c19990d8f236ec7
rohgr <rdagrawal6@gmail.com>
2022-02-21 08:25:10 +0530

    Adding assingment Day3

---
+++ src/com/java/subtract.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class subtract {
+
+        public static void main (String[]args){
+            int a=10,b=6,c=0;
+            c = a-b;
+            System.out.println(c);
+
+
+        }
+}
